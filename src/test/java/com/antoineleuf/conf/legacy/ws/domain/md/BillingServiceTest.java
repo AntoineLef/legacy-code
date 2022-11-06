93d987a00ccce555af00ea86a6960845ce858174
Antoine Lefrancois <antoine.lefrancois@outlook.com>
2022-11-06 12:29:50 -0500

    Added unit test cases missing

--- src/test/java/com/antoineleuf/conf/legacy/ws/domain/md/BillingServiceTest.java
+++ src/test/java/com/antoineleuf/conf/legacy/ws/domain/md/BillingServiceTest.java
@@ -19,6 +19,7 @@ import com.google.common.truth.Truth;
 
 public class BillingServiceTest {
 
+  private static final double CHARGE_FOR_EIGHT_HOURS = 600.0;
   private static final LocalDate TODAY = LocalDate.now();
   private static final String A_DOCTOR_ID = "A_DOCTOR_ID";
   private static PreparedStatement insert;
@@ -147,7 +148,58 @@ public class BillingServiceTest {
     Double amount = service.calculatePayRatioFrom(startDateTime, endDateTime);
 
     // then
-    Truth.assertThat(amount).isEqualTo(600.0);
+    Truth.assertThat(amount).isEqualTo(CHARGE_FOR_EIGHT_HOURS);
   }
 
+  @Test
+  public void given1HourWorked_whenCalculatingPayForWorkedHours_thenPayIsOneEightOfFullDay() {
+    // given
+    LocalDateTime startDateTime = LocalDateTime.parse("2022-11-05T10:00:00");
+    LocalDateTime endDateTime = LocalDateTime.parse("2022-11-05T11:00:00");
+
+    // when
+    Double amount = service.calculatePayRatioFrom(startDateTime, endDateTime);
+
+    // then
+    Truth.assertThat(amount).isEqualTo(CHARGE_FOR_EIGHT_HOURS / 8);
+  }
+
+  @Test
+  public void givenZeroHourWorked_whenCalculatingPayForWorkedHours_thenPayIsZero() {
+    // given
+    LocalDateTime startDateTime = LocalDateTime.parse("2022-11-05T10:00:00");
+    LocalDateTime endDateTime = LocalDateTime.parse("2022-11-05T10:00:00");
+
+    // when
+    Double amount = service.calculatePayRatioFrom(startDateTime, endDateTime);
+
+    // then
+    Truth.assertThat(amount).isEqualTo(0.0);
+  }
+
+  @Test
+  public void givenMoreThanEightHoursWorked_whenCalculatingPayForWorkedHours_thenPayIsMoreThanFullDay() {
+    // given
+    LocalDateTime startDateTime = LocalDateTime.parse("2022-11-05T10:00:00");
+    LocalDateTime endDateTime = LocalDateTime.parse("2022-11-05T20:00:00");
+
+    // when
+    Double amount = service.calculatePayRatioFrom(startDateTime, endDateTime);
+
+    // then
+    Truth.assertThat(amount).isEqualTo(CHARGE_FOR_EIGHT_HOURS * 1.25);
+  }
+
+  @Test
+  public void givenEightHoursWorkedBetweenTwoDays_whenCalculatingPayForWorkedHours_thenPayIsFullDay() {
+    // given
+    LocalDateTime startDateTime = LocalDateTime.parse("2022-11-04T20:00:00");
+    LocalDateTime endDateTime = LocalDateTime.parse("2022-11-05T04:00:00");
+
+    // when
+    Double amount = service.calculatePayRatioFrom(startDateTime, endDateTime);
+
+    // then
+    Truth.assertThat(amount).isEqualTo(CHARGE_FOR_EIGHT_HOURS);
+  }
 }
