5d940de976ea59d907637e22d6a17244dc937513
Antoine Lefrancois <antoine.lefrancois@outlook.com>
2022-11-05 22:10:15 -0400

    Tested no days is counted except wanted date

--- src/test/java/com/antoineleuf/conf/legacy/ws/domain/md/BillingServiceTest.java
+++ src/test/java/com/antoineleuf/conf/legacy/ws/domain/md/BillingServiceTest.java
@@ -15,15 +15,18 @@ import org.junit.jupiter.api.Test;
 
 public class BillingServiceTest {
 
+  private static final LocalDate TODAY = LocalDate.now();
+  private static final String A_DOCTOR_ID = "A_DOCTOR_ID";
   private static PreparedStatement insert;
   private static Connection c;
+  private BillingService service = new BillingService();
 
   @BeforeAll
   public static void before() throws SQLException {
     c = DriverManager.getConnection("jdbc:postgresql://localhost:49153/doctordb", "postgres", "postgrespw");
 
-    insert =
-           c.prepareStatement("INSERT INTO procedures values ('TEST_PROCEDURE_ID', 'A_DOCTOR_ID', 'AN_HOSPITAL', ?, ?, ?);");
+    insert = c.prepareStatement("INSERT INTO procedures values ('TEST_PROCEDURE_ID', 'A_DOCTOR_ID',"
+                                + " 'AN_HOSPITAL', ?, ?, ?);");
 
   }
 
@@ -35,29 +38,65 @@ public class BillingServiceTest {
   @AfterEach
   public void deleteTestRecord() throws SQLException {
 
-    PreparedStatement deleteTestProcedure =
-                                          c.prepareStatement("DELETE from procedures where p_id like 'TEST_PROCEDURE_ID';");
+    PreparedStatement deleteTestProcedure = c.prepareStatement("DELETE from procedures where p_id like "
+                                                               + "'TEST_PROCEDURE_ID';");
     deleteTestProcedure.execute();
   }
 
   @Test
-  public void givenAFullDayWorked_whenCalculatingPay_thenPayIsFull() throws SQLException {
+  public void givenAFullDayWorked_whenCalculatingPayForToday_thenPayIsFull() throws SQLException {
     // given
     createAFullDayOfWork();
 
-    BillingService service = new BillingService();
-
     // when
-    double dailyTotal = service.dailyTotalOf("A_DOCTOR_ID", LocalDate.now());
+    double dailyTotal = service.dailyTotalOf(A_DOCTOR_ID, TODAY);
 
     // then
     assertEquals(600.0, dailyTotal);
   }
 
+  @Test
+  public void givenAHalfDayWorked_whenCalculatingPayForToday_thenPayIsHalf() throws SQLException {
+    // given
+    createAHalfDayOfWork();
+
+    // when
+    double dailyTotalOf = service.dailyTotalOf(A_DOCTOR_ID, TODAY);
+
+    // then
+    assertEquals(300.0, dailyTotalOf);
+  }
+
+  @Test
+  public void givenAFullDayWorkedYesterday_whenCalculatingPayForToday_thenPayIsZero() throws SQLException {
+    // given
+    createAFullDayOfWorkYesterday();
+
+    // when
+    double dailyTotalOf = service.dailyTotalOf(A_DOCTOR_ID, TODAY);
+
+    // then
+    assertEquals(0.0, dailyTotalOf);
+  }
+
+  private void createAFullDayOfWorkYesterday() throws SQLException {
+    insert.setString(1, "10:00:00");
+    insert.setString(2, "18:00:00");
+    insert.setString(3, TODAY.minusDays(1).toString());
+    insert.executeUpdate();
+  }
+
+  private void createAHalfDayOfWork() throws SQLException {
+    insert.setString(1, "10:00:00");
+    insert.setString(2, "14:00:00");
+    insert.setString(3, TODAY.toString());
+    insert.executeUpdate();
+  }
+
   private void createAFullDayOfWork() throws SQLException {
     insert.setString(1, "10:00:00");
     insert.setString(2, "18:00:00");
-    insert.setString(3, LocalDate.now().toString());
+    insert.setString(3, TODAY.toString());
     insert.executeUpdate();
   }
 
